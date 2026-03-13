1b65f526c7851ce158c1df5db148694d7f25b627
Casey Burgess <caburges@student.42.fr>
2026-03-13 18:50:49 +0100

    p3: working to launch argocd manually, need to make deploy.sh work

--- p3/deploy.sh
+++ p3/deploy.sh
@@ -1,6 +1,19 @@
 #!/bin/bash 
-k3d cluster create p3 -p "8888:30000@loadbalancer"
+k3d cluster create p3 \
+  -p "8888:30000@loadbalancer"
 
-#
-kubectl apply -f confs
+kubectl create namespace argocd
+
+kubectl apply -n argocd --server-side --force-conflicts \
+  -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+echo "Waiting for Argo CD server . . ."
+kubectl wait --for=condition=Ready pods --all -n argocd --timeout=10s
+
+# Redirection that works only after argocd is up
+kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+
+echo "Ready! Available at https://localhost:8080"
+echo "Try curl https://localhost:8080 --insecure"
+# kubectl apply -f confs
 # TODO: add wait for it to be ready before applying confs
--- scripts/install-k3d.sh
+++ scripts/install-k3d.sh
@@ -9,3 +9,9 @@ install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 # Install k3d
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
+# TODO organize files and decide if we install argo in p3
+
+curl -sSL -o argocd-linux-amd64 \
+	https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
