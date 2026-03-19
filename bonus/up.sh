b7d2adf43a4fb913de55098491cd436a3274eb39
Ricardo Aranguren Gassis <rarangur@student.42.fr>
2026-03-19 17:07:44 +0100

    print gitlab root password

--- bonus/up.sh
+++ bonus/up.sh
@@ -10,3 +10,17 @@ helm upgrade --install gitlab gitlab/gitlab \
   --timeout 600s \
   -f values.yaml --namespace=gitlab
 
+kubectl rollout status deployment gitlab-webservice-default -n gitlab --timeout=300s
+
+echo Waiting for Gitlab web service . . .
+until curl -k http://gitlab.localhost:8081 >/dev/null 2>&1; do
+  sleep 1
+done
+
+ROOT_PASSWORD=$(kubectl -n gitlab get secret gitlab-gitlab-initial-root-password \
+  -o jsonpath="{.data.password}" | base64 -d)
+
+echo "Root password:"
+echo "$ROOT_PASSWORD"
+
+#TODO login (?) generate git repo etc.
